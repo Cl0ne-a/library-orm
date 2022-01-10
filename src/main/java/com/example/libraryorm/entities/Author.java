7cfa5a85426dbb52709dab1b1b526075f638ebb6
Anna <unit10@protonmail.com>
2022-01-11 00:49:59 +0200

    lazy initialization fixed

--- src/main/java/com/example/libraryorm/entities/Author.java
+++ src/main/java/com/example/libraryorm/entities/Author.java
@@ -10,14 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.NamedAttributeNode;
-import javax.persistence.NamedEntityGraph;
 
-//@NamedEntityGraph(
-//        name = "author-graph",
-//        attributeNodes = {
-//                @NamedAttributeNode("name")
-//        })
 @Entity
 @Data
 @Builder
